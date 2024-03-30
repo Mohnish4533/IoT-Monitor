4943cf295e5a8aa5e3504d95037bd78298303015
Mohnish4533 <mv4533@srmist.edu.in>
2024-03-30 07:14:47 +0530

    Added code to receive streamed video feed and to capture moments

--- viewing.py
+++ viewing.py
@@ -12,3 +12,27 @@ args.add_argument("-p", "--port", type=str, default="5555",
                   help="port number of the server to which the client will connect")
 
 args = vars(args.parse_args())
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.SUB)
+footage_socket.bind('tcp://' + args["ip"] + ':' + args["port"])
+footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode_(''))
+
+while True:
+    try:
+        frame = footage_socket.recv()
+        npimg = np.frombuffer(frame, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+        cv2.imshow("Stream", source)
+        k=cv2.waitKey(1)
+        if k%256==27:
+            print("STOPPING")
+            break
+        elif k%256 == 32:
+            print("capturing image\n")
+            img_name="samples/{}.png".format(datetime.datetime.now().strftime("%A-%H.%M.%S"))
+            cv2.imwrite(img_name,source)
+
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+        break
