83e30ad8d25829c94d610b7280bffb41617e7ae4
Mohnish4533 <mv4533@srmist.edu.in>
2024-03-30 06:17:08 +0530

    Added code for streaming video feed

--- streaming.py
+++ streaming.py
@@ -40,6 +40,29 @@ def brute_check(nc):
         print(f"Available cameras: {camera}")
 
 
+# main program starts here
+        
+brute_check(args["num_camera"])
+
+camera = cv2.VideoCapture(camera[0])
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://{}:{}'.format(args["ip"], args["port"]))
+
+while True:
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        #frame = cv2.resize(frame, (640, 480))  # resize the frame
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        # jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(buffer)
+
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
+
 if __name__ == "__main__":
      print(args["num_camera"])
      brute_check(args["num_camera"])
